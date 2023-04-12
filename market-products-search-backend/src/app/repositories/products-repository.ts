cbfb5281da5a54e158322516b691c8e3408ca410
Caique Ferian <caiquegsf@gmail.com>
2023-04-12 17:02:35 -0300

    Cria todos use cases da API

--- market-products-search-backend/src/app/repositories/products-repository.ts
+++ market-products-search-backend/src/app/repositories/products-repository.ts
@@ -3,5 +3,5 @@ import { ProductMeli } from '@app/entities/productMeli';
 export default abstract class ProductRepository {
   abstract create(product: ProductBuscape | ProductMeli): Promise<void>;
   abstract getAll(): Promise<ProductBuscape[] | ProductMeli[]>;
-  abstract filter(type: string): Promise<ProductBuscape[] | ProductMeli[]>;
+  abstract filter(query: string): Promise<ProductBuscape[] | ProductMeli[]>;
 }
