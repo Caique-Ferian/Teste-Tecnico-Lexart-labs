0790d6f2fcc064c249ae12039c4a29d7dc08a59d
Caique Ferian <caiquegsf@gmail.com>
2023-04-17 19:34:19 -0300

    TESTANDO

--- market-products-search-backend/src/main.ts
+++ market-products-search-backend/src/main.ts
@@ -4,7 +4,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: '*' });
+  app.enableCors({
+    origin: [
+      'https://localhost:3000',
+      'https://market-products-search-frontend.up.railway.app/',
+    ],
+    methods: ['GET', 'POST'],
+    credentials: true,
+  });
   await app.listen(3001);
 }
 bootstrap();
