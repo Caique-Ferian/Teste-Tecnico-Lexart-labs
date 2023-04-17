e50e87523c08f074590c658c6d178d111f9e3463
Caique Ferian <caiquegsf@gmail.com>
2023-04-17 19:28:28 -0300

    TESTANDO

--- market-products-search-backend/src/main.ts
+++ market-products-search-backend/src/main.ts
@@ -1,10 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Envs } from './config/config.service';
+// import { Envs } from './config/config.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: '*' });
-  await app.listen(process.env.APP_PORT || Envs.appPort() || 3001);
+  await app.listen(3001);
 }
 bootstrap();
