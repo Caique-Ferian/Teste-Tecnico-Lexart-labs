05aeabcafe4ea6af4d8e9ec8f06c3046467fab37
Caique Ferian <caiquegsf@gmail.com>
2023-04-17 15:59:38 -0300

    Adiciona docker-compose.yml e configura variaveis de ambiente no NEST

--- market-products-search-backend/src/main.ts
+++ market-products-search-backend/src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Envs } from './config/config.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  await app.listen(3001);
+  await app.listen(Envs.appPort() || 3001);
 }
 bootstrap();
